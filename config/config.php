cd1e632c13b8162bff8850005c35eca03bbd308c
Barnabás Mátis <jr.barna@gmail.com>
2024-04-09 15:39:36 +0200

    minor changes, because I'm dumb

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'url_base' => env('UCRM_URL_BASE','https://ucrm.example.com'),
-    'url_path' => env('UCRM_URL_PATH','/api/v1.0/'),
+    'url_base' => env('UCRM_URL_BASE'),
+    'url_path' => env('UCRM_URL_PATH'),
     'token' => env('UCRM_TOKEN'),
     'payment_method' => env('UCRM_PAYMENT_METHOD'),
     'payment_user_id' => env('UCRM_PAYMENT_USER_ID'),
